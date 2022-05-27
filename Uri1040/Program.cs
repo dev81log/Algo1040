918b3fa8e926efa25ca2564839136010c3a08f8e
dev81log <dev81log@gmail.com>
2022-05-26 23:52:34 -0300

    Estudo de algoritimo

---
+++ Uri1040/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Globalization;
+
+namespace Uri1040;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        var cv = CultureInfo.InvariantCulture;
+
+        var n1 = double.Parse(Console.ReadLine(), cv);
+        var n2 = double.Parse(Console.ReadLine(), cv);
+        var n3 = double.Parse(Console.ReadLine(), cv);
+        var n4 = double.Parse(Console.ReadLine(), cv);
+
+        var media = (n1 + n2 + n3 + n4) / 4;
+        Console.WriteLine("Media: " + media.ToString("F1", cv));
+
+        switch (media)
+        {
+            case >= 7.0:
+                Console.WriteLine("Aluno aprovado.");
+                break;
+            case <= 5.0:
+                Console.WriteLine("Aluno reprovado.");
+                break;
+            default:
+            {
+                if (media is >= 5.0 and <= 6.9)
+                {
+                    Console.WriteLine("Aluno em exame.");
+                    var mediaExame = double.Parse(Console.ReadLine(), cv);
+                    var novaMedia = (media + mediaExame) / 2;
+                    Console.WriteLine("Nota do exame: " + novaMedia.ToString("F1", cv));
+
+                    switch (novaMedia)
+                    {
+                        case >= 5.0:
+                            Console.WriteLine("Aluno aprovado.");
+                            break;
+                        case <= 4.9:
+                            Console.WriteLine("Aluno reprovado");
+                            break;
+                        default:
+                            Console.WriteLine("Erro ao achar esta média!");
+                            break;
+                    }
+                }
+                break;
+            }
+        }
+    }
+}
